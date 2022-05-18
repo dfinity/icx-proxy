12727261f95f4b77da266c6b8879fd44275a76b9
Daniel.Bloom <daniel.bloom@dfinity.org>
2022-05-18 10:26:53 -0700

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,10 @@ use hyper::{
 };
 use ic_agent::{
     agent::http_transport::{reqwest, ReqwestHttpReplicaV2Transport},
+    agent_error::HttpErrorPayload,
     export::Principal,
     ic_types::{hash_tree::LookupResult, HashTree},
-    lookup_value, Agent, AgentError, Certificate, agent_error::HttpErrorPayload,
+    lookup_value, Agent, AgentError, Certificate,
 };
 use ic_utils::{
     call::AsyncCall,
@@ -401,12 +402,16 @@ async fn forward_request(
                 .status(StatusCode::INTERNAL_SERVER_ERROR)
                 .body(format!(r#"Replica Error ({}): "{}""#, reject_code, reject_message).into())
                 .unwrap())),
-            Err(AgentError::HttpError(HttpErrorPayload{status: 451, content_type, content})) => Err(Ok(
-                content_type.into_iter().fold(Response::builder(), |r, c| r.header(CONTENT_TYPE, c))
+            Err(AgentError::HttpError(HttpErrorPayload {
+                status: 451,
+                content_type,
+                content,
+            })) => Err(Ok(content_type
+                .into_iter()
+                .fold(Response::builder(), |r, c| r.header(CONTENT_TYPE, c))
                 .status(451)
                 .body(content.into())
-                .unwrap()
-            )),
+                .unwrap())),
             Err(e) => Err(Err(e.into())),
         }
     }
