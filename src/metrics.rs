4f732947846f3314e73db185e2932ad5a05c37b3
Daniel Bloom <82895745+Daniel-Bloom-dfinity@users.noreply.github.com>
2022-09-30 09:14:04 -0700

    bump `ic-agent`, `clap` and release (#48)

--- src/metrics.rs
+++ src/metrics.rs
@@ -3,7 +3,11 @@ use std::{future::Future, net::SocketAddr};
 use axum::{handler::Handler, routing::get, Extension, Router};
 use clap::Args;
 use futures::{future::OptionFuture, FutureExt};
-use ic_agent::{ic_types::Principal, Agent};
+use ic_agent::{
+    agent::http_transport::hyper::{self, Body, Request, Response, StatusCode, Uri},
+    ic_types::Principal,
+    Agent,
+};
 use opentelemetry::{
     global,
     metrics::{Counter, Meter},
@@ -13,7 +17,6 @@ use opentelemetry::{
 use opentelemetry_prometheus::PrometheusExporter;
 use prometheus::{Encoder, TextEncoder};
 
-use crate::http_transport::hyper::{self, Body, Request, Response, StatusCode, Uri};
 use crate::{headers::HeadersData, logging::add_trace_layer, validate::Validate};
 
 /// The options for metrics
